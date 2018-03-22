f8d8094cae7ed618478fede03e4e8adb7daa8da0
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-03-22 17:29:41 +0900

    Refactor

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Field, reduxForm } from 'redux-form'
+import {Field, reduxForm} from 'redux-form'
 
 const Form = (props) => {
   const {hundleSubmit, pristine, reset, submitting} = props
@@ -8,26 +8,52 @@ const Form = (props) => {
       <div>
         <label>First Name</label>
         <div>
-          <Field name="firstName" component="input" type="text" placeholder="First Name"/>
+          <Field
+            name="firstName"
+            component="input"
+            type="text"
+            placeholder="First Name"/>
         </div>
       </div>
       <div>
         <label>Last Name</label>
         <div>
-          <Field name="lastName" component="input" type="text" placeholder="Last Name"/>
+          <Field
+            name="lastName"
+            component="input"
+            type="text"
+            placeholder="Last Name"/>
         </div>
       </div>
       <div>
         <label>Email</label>
         <div>
-          <Field name="email" component="input" type="email" placeholder="Email"/>
+          <Field
+            name="email"
+            component="input"
+            type="email"
+            placeholder="Email"/>
         </div>
       </div>
       <div>
         <label>Sex</label>
         <div>
-          <label><Field name="sex" component="input" type="radio" value="male"/>Mele</label>
-          <label><Field name="sex" component="input" type="radio" value="female"/>Female</label>
+          <label>
+            <Field
+              name="sex"
+              component="input"
+              type="radio"
+              value="male"/>
+            Mele
+          </label>
+          <label>
+            <Field
+              name="sex"
+              component="input"
+              type="radio"
+              value="female"/>
+            Female
+          </label>
         </div>
       </div>
       <div>
